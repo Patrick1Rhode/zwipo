2236a7df572b99078188a696ebfd1b72f66e932d
Sikalinda Patrick <sikalindapatrick@gmail.com>
2016-08-26 01:58:20 +0100

    wow

--- routes/register.js
+++ routes/register.js
@@ -6,15 +6,14 @@ router.get("/",function(req,res){
           var firstName = req.param("Fname");
           var lastName = req.param("Sname");
           var phone = req.param("phone");
-        //dbwork
-       // {fname : "Mwiza",sname : "Simbeye", phone : "260965175641", id : "260965175641"}
+       
        
         
         
         
         r.db("Zinga").table("users").insert({fname : firstName,lname : lastName , key : phone}).run().then(function(data){
-            console.log(jsondata);
-            console.log(data.first_error+" from success");
+        
+            
         }).error(function(error){
             console.log(error.first_error+" from error");
         });
